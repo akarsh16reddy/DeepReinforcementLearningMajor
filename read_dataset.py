b1601fa097740274edc2573cfd1978846182e272
Karsh <63505953+akarsh16reddy@users.noreply.github.com>
2022-03-11 23:26:11 +0530

    Add files via upload

---
+++ read_dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_excel('C:\Major\dataset.xlsx');
+
+temp_df = df.loc[df['timestamp']==0]
+
+temp_df1 = temp_df[['timestamp','rounded_load','type']]
+
+temp_df2 = df.loc[:]
+'''
+for row in temp_df2.itertuples(index=True, name='Pandas'):
+      print(row)
+      '''
+def get0TimeStepJobs():
+    return temp_df2;
