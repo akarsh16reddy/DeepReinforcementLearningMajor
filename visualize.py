2f1e316e3e0151ab74bcb12664612564ea116ef2
Karsh <63505953+akarsh16reddy@users.noreply.github.com>
2022-03-20 17:20:39 +0530

    Random + round robin graphs implemented

--- visualize.py
+++ visualize.py
@@ -24,11 +24,33 @@ for key,value in dictionary.items():
         averageJobResponseTimes.insert(key,mean(value))
     else:
         averageJobResponseTimes.insert(key,0)
-        
-plt.plot(timeStepList,averageJobResponseTimes, color='y')
+
+plt.plot(timeStepList,averageJobResponseTimes, color='y', label="random")
 plt.xlabel("Timestep")
 plt.ylabel("Avg job response time")
-plt.title("Average Job Response Times - Random")
+plt.title("Average Job Response Times")
+
+df = pd.read_csv('responsetimes_roundrobin.csv');
+
+dictionary = {}
+for i in range(198):
+    dictionary[i]=[]
+
+for i in range(0,198,1):
+    for row in df.loc[df['arrivalT']==i].itertuples(index=True,name='Pandas'):
+        dictionary[row.arrivalT].append(row.responseTime)
+
+averageJobResponseTimes_rr = []
+numberOfRequests_rr = []
+timeStepList_rr = []
+for key,value in dictionary.items():
+    numberOfRequests_rr.insert(key,len(value))
+    timeStepList_rr.append(key)
+    if len(value)!=0:
+        averageJobResponseTimes_rr.insert(key,mean(value))
+    else:
+        averageJobResponseTimes_rr.insert(key,0)
+plt.plot(timeStepList_rr,averageJobResponseTimes_rr, color='b',label="round robin")
   
 plt.legend()
   
